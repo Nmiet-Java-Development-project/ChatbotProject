e05c78b4ecca0b57e37614d7a9948008713a1379
technicallyengineer <141140000+technicallyengineer@users.noreply.github.com>
2023-10-15 21:30:09 +0530

    GUI updated

--- app/src/main/java/com/example/chatbot/MainActivity.java
+++ app/src/main/java/com/example/chatbot/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -15,18 +21,51 @@ public class MainActivity extends AppCompatActivity {
     EditText messageEditText;
     ImageButton sendButton;
 
+    List<Message> messagelist;
+    MessageAdapter messageAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messagelist = new ArrayList<>();
+
+
         recyclerView = findViewById(R.id.recycler_view);
         welcomeTextView = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.message_edit_text);
         sendButton = findViewById(R.id.send_btn);
-        sendButton.setOnClickListener(v->{
+
+        //setup recycler view
+
+        messageAdapter = new MessageAdapter(messagelist);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener((v)->{
             String question = messageEditText.getText().toString().trim();
-            Toast.makeText(this,question, Toast.LENGTH_LONG).show();
+            addtoChat(question, Message.SENT_BY_ME);
+            messageEditText.setText("");
+            welcomeTextView.setVisibility(View.GONE);
         });
+
+    }
+
+//    To  Add message to messageList
+    void addtoChat(String message, String sentBy){
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messagelist.add(new Message(message,sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+
+
 
     }
 }
